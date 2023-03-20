4bb22602acbb0f4750b34bedf5907a680896f8d5
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-03-20 12:54:35 +0100

    solved binary_tree_depth

---
+++ binary_tree_depth.py
@@ -0,0 +1,22 @@
+"""
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path 
+from the root node down to the farthest leaf node.
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def max_depth(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+
+    return max(max_depth(root.left) + 1, max_depth(root.right) + 1)
