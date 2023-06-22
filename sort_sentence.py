a9272fae02a58bb3aa8c06ffa8d4138b22bbaec9
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-06-22 16:20:47 +0200

    solved sort_sentence

---
+++ sort_sentence.py
@@ -0,0 +1,16 @@
+"""
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. 
+Each word consists of lowercase and uppercase English letters.
+
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+"""
+
+def sortSentence(s: str) -> str:
+
+    groups = s.split(" ")
+    groups.sort(key=lambda w: int(w[-1]))
+
+    return " ".join(w[:-1] for w in groups)
