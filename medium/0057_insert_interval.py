ee50d514cd625997f551d03f23610da0babf8254
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-07-16 16:25:26 +0200

    added explanation to 0057_insert_interval

--- medium/0057_insert_interval.py
+++ medium/0057_insert_interval.py
@@ -7,26 +7,30 @@ Insert newInterval into intervals such that intervals is still sorted in ascendi
 still does not have any overlapping intervals (merge overlapping intervals if necessary).
 
 Return intervals after the insertion.
+
+
+EXPLANATION:
+
+We need to realize the following things:
+
+- If the start of the interval is greater than the end of our new interval, we need to insert it and return, no merge
+- If the end of the interval is smaller than the start of our new interval, there is no merge so we just add the interval
+- When we merge an interval with our new interval, the start will be the min of both and the end will be the max of both
 """
 
 
 def insert(intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-    new_start, new_end = newInterval
     result = []
-    inserted = False
-
-    for start, end in intervals:
-        if inserted or end < new_start:
-            result.append([start, end])
-        elif start > new_end:
-            result.append([new_start, new_end])
-            result.append([start, end])
-            inserted = True
+    
+    for i, (start, end) in enumerate(intervals):
+        new_start, new_end = newInterval
+        if start > new_end:
+            result.append(newInterval)
+            return result + intervals[i:]
+        elif end < new_start:
+            result.append(intervals[i])
         else:
-            new_start = min(start, new_start)
-            new_end = max(end, new_end)
-
-    if not inserted:
-        result.append([new_start, new_end])
-
+            newInterval = [min(start, new_start), max(end, new_end)]
+    
+    result.append(newInterval)
     return result
