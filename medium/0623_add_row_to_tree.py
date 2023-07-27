d8aa06db6f3921b0af5f6468a7db5a314af3a865
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-07-27 21:55:58 +0200

    0623_add_row_to_tree

--- medium/0623_add_row_to_tree.py
+++ medium/0623_add_row_to_tree.py
@@ -21,3 +21,42 @@ for each node at that depth.
 
 Only edgecases to handle are root being None or depth being 1 since we need to create / replace root.
 """
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+def addOneRow(root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+    if not root:
+        return TreeNode(val)
+
+    if depth == 1:
+        new_root = TreeNode(val)
+        new_root.left = root
+        return new_root
+        
+    current_depth = 0
+    level = [root]
+
+    while level:
+        current_depth += 1
+        next_level = []
+
+        for node in level:
+            if current_depth == depth - 1:
+                for node in level:
+                    node.left = TreeNode(val, node.left)
+                    node.right = TreeNode(val, None, node.right)
+                break
+
+            if node.left:
+                next_level.append(node.left)
+            if node.right:
+                next_level.append(node.right)
+
+        level = next_level
+
+    return root
